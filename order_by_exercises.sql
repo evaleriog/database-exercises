4e7eed223167f093ea81c21abf19f9758ebd0b95
Eduardo Valerio Gamez <evaleriog@outlook.com>
2019-12-18 16:15:12 -0600

    Order By Exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,10 +2,14 @@ USE employees;
 
 /*Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows*/
 /*Modify your first query to order by first name. The first result should be Irena Reutenauer and the last result should be Vidya Simmen.*/
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+/*Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig*/
+/*Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda*/
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC, first_name DESC;
 
 /*Find all employees whose last name starts with 'E' — 7,330 rows. */
-SELECT * FROM employees WHERE last_name LIKE 'E%';
+/*Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!*/
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
 
 /*Find all employees hired in the 90s — 135,214 rows.*/
 SELECT  * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
@@ -29,7 +33,8 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
 /*Find all employees hired in the 90s and born on Christmas — 362 rows.*/
-SELECT  * FROM employees WHERE birth_date LIKE '%-12-25' AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31');
+/*Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.*/
+SELECT  * FROM employees WHERE birth_date LIKE '%-12-25' AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31') ORDER BY birth_date, hire_date DESC;
 
 /*Find all employees with a 'q' in their last name but not 'qu' — 547 rows.*/
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
